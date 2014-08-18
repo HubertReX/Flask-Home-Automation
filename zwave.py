7d8111c1cd82cc8fc771189b3d17667c61cc2fac
Hubert Nafalski <Hubert.Nafalski@gmail.com>
2014-08-18 22:04:15 +0200

    added zwave AgoController subscribe and getevent

--- zwave.py
+++ zwave.py
@@ -1,4 +1,4 @@
-import sys, json, urllib2
+import sys, json, urllib2, datetime
 
 URL = "http://192.168.1.110:8008/jsonrpc"
 UUIDS = {
@@ -6,58 +6,84 @@ UUIDS = {
   "controller" : "c4a10ff0-6d7e-4630-ae4b-c0cd6e0af117",
   }
 
-def agocontrol_send_cmd(param, val):
-    
-    msg_on = {
-        "jsonrpc": "2.0",
-        "method": "message",
-        "params": {
-          "content": {
-            "command": "on",
-            "uuid": ""
-          }
-        },
-        "id": 1
-      }
-    msg_off = {
-        "jsonrpc": "2.0",
-        "method": "message",
-        "params": {
-          "content": {
-            "command": "off",
-            "uuid": ""
-          }
-        },
-        "id": 1
-      }
-
+def agocontrol_send(msg, timeout=1):
+    #print msg
     req = urllib2.Request(URL)
     req.add_header('Content-Type', 'application/json')
     req.add_header('Cache-Control', 'no-cache')
     
-    data = {}
-    response = None
-    if param == "speakers":
-      uuid = UUIDS[param]
-      if val == "on":
-        msg = msg_on
-      else:
-        msg = msg_off
-      
-      msg["params"]["content"]["uuid"] = uuid
-      
-      try:
-        response = urllib2.urlopen(req, json.dumps(msg), timeout=1).read()
-      except:
-        pass
+    response = ""
+    try:
+      response = urllib2.urlopen(req, json.dumps(msg), timeout=timeout).read()
+    except:
+      pass
     
     return response
 
 
-if __name__ == "__init__":
+def agocontrol_send_cmd(param, val):
+    #print param, val
+    msg = {
+        "jsonrpc": "2.0",
+        "method": "message",
+        "params": {
+          "content": {
+            "command": "",
+            "uuid": ""
+          }
+        },
+        "id": 1
+      }
+    if param == "speakers":
+      uuid = UUIDS[param]
+      msg["params"]["content"]["uuid"] = uuid
+      msg["params"]["content"]["command"] = val      
+      
+      return agocontrol_send(msg)
+    
+    return ""
+
+def agocontrol_subscribe():
+
+    msg = {
+          "method": "subscribe",
+          "id": 1,
+          "jsonrpc": "2.0"
+        }
+    print "subscribe"
+    return agocontrol_send(msg)
+
+def agocontrol_getevent(uuid=None):
+    
+    if uuid == None:
+      res = agocontrol_subscribe()
+      uuid = json.loads(res)["result"]
+
+    msg = {
+      "method": "getevent",
+      "params": {
+          "uuid": uuid
+      },
+      "id": 2,
+      "jsonrpc": "2.0"
+    }
+
+    res = agocontrol_send(msg, 10000)
+    res = json.loads(res)["result"]
+    res = json.dumps(res, indent=4, separators=(',', ': ') )
+    print datetime.datetime.now().replace(microsecond=0).time().isoformat(), res
+    
+    return (res, uuid)
+
+def main():
+  uuid=None
+  while True:
+    res, uuid = agocontrol_getevent(uuid)
+
+if __name__ == "__main__":
   if len(sys.argv) < 3:
     print "wrong parameters count"
     exit(1)
 
-    res = agocontrol_send_cmd(sys.argv[1], sys.argv[2])
-    print res
+    #res = agocontrol_send_cmd(sys.argv[1], sys.argv[2])
+    #print res
